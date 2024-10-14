250641472f087b94b101767eb6009b4d8d35e47f
Muhmmed-Gaafar <muhmmedgaafar@gmail.com>
2024-10-14 20:00:49 +0300

    update

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Conversation extends Model
 {
     use HasFactory;
@@ -14,15 +14,15 @@ class Conversation extends Model
         'last_message_id',
     ];
 
-    public function user1()
+    public function user1() : BelongsTo
     {
         return $this->belongsTo(User::class , 'user_id2');
     }
-    public function user2()
+    public function user2() : BelongsTo
     {
         return $this->belongsTo(User::class , 'user_id1');
     }
-    public function last_message()
+    public function last_message() : BelongsTo
     {
         return $this->belongsTo(Message::class , 'last_message_id');
     }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Message extends Model
 {
@@ -16,19 +19,19 @@ class Message extends Model
         'group_id',
     ];
 
-    public function sender()
+    public function sender() :BelongsTo
     {
         return $this->belongsTo(User::class, 'sender_id');
     }
-    public function receiver()
+    public function receiver() :BelongsTo
     {
         return $this->belongsTo(User::class, 'receiver_id');
     }
-    public function group()
+    public function group() :BelongsTo
     {
         return $this->belongsTo(Group::class, 'group_id');
     }
-    public function attachments()
+    public function attachments() :HasMany
     {
         return $this->hasMany(MessageAttachment::class);
     }
